e258f01ad93941a398029ae048e95b0d7d159f44
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2018-01-13 14:34:55 +0100

    wrap function call into IIFE

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-var button = document.getElementById('get-joke');
-var paragraph = document.getElementById('joke');
+(function() {
+  var button = document.getElementById('get-joke');
+  var paragraph = document.getElementById('joke');
+  var icndbJokes = getJokeFrom('http://api.icndb.com/jokes/random');
 
-button.addEventListener('click', function() {
-  joke(function(response) {
+  button.addEventListener('click', function() {
+    icndbJokes(function(response) {
+      paragraph.innerHTML = response
+    });
+  });
+
+  icndbJokes(function(response) {
     paragraph.innerHTML = response
   });
-});
 
-joke(function(response) {
-  paragraph.innerHTML = response
-});
+  function getJokeFrom(url) {
+    var xhr = new XMLHttpRequest();
 
-function getJokeFrom(url) {
-  var xhr = new XMLHttpRequest();
-
-  return function(cb) {
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function() {
-      cb(JSON.parse(xhr.response).value.joke)
-    });
-    xhr.send();
+    return function(cb) {
+      xhr.open('GET', url);
+      xhr.addEventListener('load', function() {
+        cb(JSON.parse(xhr.response).value.joke)
+      });
+      xhr.send();
+    }
   }
-}
-
-function joke(cb) {
-  getJokeFrom('http://api.icndb.com/jokes/random')(cb);
-}
+}())
