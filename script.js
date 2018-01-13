8fcbede2d533b16defdcbda4571ccbf6a8eba1b3
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2018-01-13 01:10:03 +0100

    created XHR object only once

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-var url = 'http://api.icndb.com/jokes/random';
+var joke = getJokeFrom('http://api.icndb.com/jokes/random')
 
 var button = document.getElementById('get-joke');
 var paragraph = document.getElementById('joke');
-button.addEventListener('click', function(){
-    getJoke();
+
+button.addEventListener('click', function() {
+  joke();
 });
 
-function getJoke() {
-    var xhr = new XMLHttpRequest();
+function getJokeFrom(url) {
+  var xhr = new XMLHttpRequest();
+
+  return function() {
     xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-        var response = JSON.parse(xhr.response);
-        paragraph.innerHTML = response.value.joke;
+    xhr.addEventListener('load', function() {
+      var response = JSON.parse(xhr.response);
+      paragraph.innerHTML = response.value.joke;
     });
     xhr.send();
+  }
+
 }
-getJoke();
+
+joke();
